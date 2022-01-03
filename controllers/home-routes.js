e6114aa7bddcfec83fec8e95bf12f73cb9428c9d
Ceri Trammell <cerihaf@gmail.com>
2022-01-03 17:46:12 -0600

    initial comment handlebars setup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,19 @@ const sequelize = require('../config/connection');
 const { User, Comment, Vote, Matchup } = require('../models');
 const animal_1 = 'Grizzly Bear';
 const animal_2 = 'Tiger';
+const comments = [{
+  comment: "This sucks!",
+  color: 2,
+  matchup_id: 2, //3 of these for now
+  author: "Huntikins" //5 user seeds
+},
+{
+  comment: "RHINOS RULE!!!!",
+  color: 1,
+  matchup_id: 2,
+ author: "huntikins"
+},]
+
 
 router.get('/', (req, res) => {
 //  if (!req.session.loggedIn) {
@@ -10,7 +23,7 @@ router.get('/', (req, res) => {
 //    return;
 //  }
 const date = new Date().getFullYear()
-  res.render('home', {layout:'main', date, animal_1, animal_2});
+  res.render('home', {layout:'main', date, animal_1, animal_2, comments});
 });
 
 // directs user to login page
