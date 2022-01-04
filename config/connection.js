6dcb1b30269d48302b8eec0917f21ced8d1d50ae
Ceri Trammell <cerihaf@gmail.com>
2022-01-03 21:04:52 -0600

    conflict changes

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize('animalarena_db', 'root', 'password', {
-     // host: process.env.DB_HOST,
-      dialect: 'mysql'//,
-     // port: process.env.DB_PORT
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: process.env.DB_PORT
 });
 
 module.exports = sequelize;
