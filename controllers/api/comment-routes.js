78a1bb90740cbd5722942ec211f230b12e3561d0
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-01-03 18:28:44 -0600

    starting on req.session

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -6,22 +6,22 @@ const auth = require("../../utils/auth");
 //expects: { "comment": STRING, "matchup_id" : INT, "user_id": INT }
 router.post("/", auth, (req, res) => {
   //vote on a matchup (triggered in a client side js script)
-  const { comment, matchup_id, user_id } = req.body;
-  Vote.findOne({ where: { matchup_id: matchup_id, user_id: user_id } }).then(
+  const { comment, matchup_id } = req.body;
+  Vote.findOne({ where: { matchup_id: matchup_id, user_id: req.session.id } }).then(
     (voteSearch) => {
       if (voteSearch !== null) {
         //user has voted on this matchup, can comment
         //1 = blue, 2 = red (rendered client side)
         if (voteSearch.vote === 1 || 2) {
           color = voteSearch.vote;
-          User.findOne({ where: { id: user_id } })
+          User.findOne({ where: { id: req.session.id } })
             .then((userSearch) => {
               const username = userSearch.username;
               Comment.create({
                 comment: comment,
                 color: color,
                 matchup_id: matchup_id,
-                user_id: user_id,
+                user_id: req.session.user_id,
                 username: username,
               }) //**do a matchup route get request after this, it will render comments and votes**
                 .then((commentData) => res.json(commentData))
