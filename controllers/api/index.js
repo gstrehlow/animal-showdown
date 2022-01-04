26d4a5663f4fdb826729258d3585a3849bae10e8
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-01-03 18:34:50 -0600

    added user route to index.js in api dir

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,9 @@ const router = require("express").Router();
 const commentRoutes = require("./comment-routes");
 const voteRoutes = require("./vote-routes");
 const matchupRoutes = require("./matchup-routes");
+const userRoutes = require("./user-routes");
 
+router.user("/users", userRoutes);
 router.use("/comments", commentRoutes);
 router.use("/votes", voteRoutes);
 router.use("/matchups", matchupRoutes);
