1a641be1a23007346f45a59048708f0a58d267db
Ceri Trammell <cerihaf@gmail.com>
2022-01-04 22:30:03 -0600

    troubleshooting the vote route. itvote API is still down

--- public/javascript/matchup.js
+++ public/javascript/matchup.js
@@ -1,9 +1,9 @@
 async function matchupFormHandler(event) {
     event.preventDefault();
-
+    console.log({...event.target.dataset})
     const response = await fetch("/api/votes", {
         method: "POST",
-        body: JSON.stringify(event.target.dataset)
+        body: JSON.stringify({...event.target.dataset})
     })
     if (!response.ok) {
         return;
