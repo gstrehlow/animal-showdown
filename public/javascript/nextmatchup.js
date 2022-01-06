96afe36059cfff3860c2982c3237721388b6b697
George Stockhammer <sunchipzzz@gmail.com>
2022-01-05 20:18:58 -0600

    fixed handlebars.js matchup_id typo, made description.js hasVoted only

--- public/javascript/nextmatchup.js
+++ public/javascript/nextmatchup.js
@@ -7,14 +7,14 @@ function nextFormHandler(event) {
     localStorage.setItem("seenMatchups",JSON.stringify(seenList));
     let target = undefined;
     for (let i = 1; i < 13; i++){
-    if (seenList[i] === undefined){
-        target = i;
-        break;
-    }
+        if (seenList[i] === undefined){
+            target = i;
+            break;
+        }
     }
     if (target === undefined){
-    target = Math.floor(Math.random() * 12) + 1;
-    localStorage.setItem("seenMatchups",JSON.stringify({})); //cleared list
+        target = Math.floor(Math.random() * 12) + 1;
+        localStorage.setItem("seenMatchups",JSON.stringify({})); //cleared list
     }
     document.location.replace(`/api/matchups/${target}`); //reload the same matchup
 }
