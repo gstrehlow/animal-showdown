e83ab995e212fb7890b5d02d4d4e4365a996959f
George Stockhammer <sunchipzzz@gmail.com>
2022-01-28 11:53:05 -0600

    removed email fields and added censorship

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -9,6 +9,19 @@ async function commentFormHandler(event) {
   ];
   console.log(comment, matchup_id);
   if (comment) {
+    if (comment.length > 250) return alert('Comments should be no more than 250 characters!');
+    const naughtyWords = ["nigg","cunt","cum","fag","fuck","shit","asshole","pussy"];
+    const censorChars = ["!","@","#","$","%","&","*"];
+    let censorString = "";
+    for (let i = 0; i < naughtyWords.length; i++){
+      if (comment.search(naughtyWords[i]) !== -1){
+        censorString = "";
+        for (let c = 0; naughtyWords[i].length; c++){
+          censorString += censorChars[Math.floor(Math.random()*censorChars.length)];
+        }
+        comment = comment.replaceAll(naughtyWords[i],censorString);
+      }
+    }
     const response = await fetch("/api/comments", {
       method: "POST",
       body: JSON.stringify({ comment, matchup_id }),
